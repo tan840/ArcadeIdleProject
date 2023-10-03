3cf7dbca2af93ad8d27f7f5768159ffd9d47724a
Tanvir Ahmed <42252274+tan840@users.noreply.github.com>
2023-10-04 01:34:56 +0600

    Added Toony Shader , Animation and fixed player movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,29 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class Movement : MonoBehaviour
 {
+    [Header("Control")]
     [SerializeField] DynamicJoystick m_Joystick;
     [SerializeField] float m_MoveSpeed = 10;
     CharacterController m_CharacterController;
+    PlayerAnimator m_PlayerAnim;
     void Start()
     {
         m_CharacterController = GetComponent<CharacterController>();
+        m_PlayerAnim = GetComponent<PlayerAnimator>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         Move();
     }
     void Move()
     {
+        Vector3 m_speed = Vector3.zero;
         if (m_Joystick.MoveThreshold > 0.05f)
         {
             Vector3 Direction = new Vector3(m_Joystick.Direction.x, 0, m_Joystick.Direction.y);
-            Vector3 m_speed = m_MoveSpeed * Direction.normalized;
+            m_speed = m_MoveSpeed * new Vector3(m_Joystick.Horizontal, 0 , m_Joystick.Vertical) * Time.fixedDeltaTime;
             m_CharacterController.SimpleMove(m_speed);
             transform.forward = Direction.normalized;
+            m_PlayerAnim.SetAnim(m_speed);
+        }
+        else
+        {
+            m_PlayerAnim.SetAnim(m_speed);
         }
     }
 }
---
+++ Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    [SerializeField] Animator m_Anim;
+    [SerializeField] float m_RunSpeedMultiplier;
+    void Start()
+    {
+        if (m_Anim == null)
+        {
+            m_Anim = GetComponent<Animator>();
+        }
+    }
+    public void SetAnim(Vector3 _Movement)
+    {
+        if (_Movement.magnitude > 0)
+        {
+            m_Anim.SetFloat("MoveSpeed", _Movement.magnitude * m_RunSpeedMultiplier);
+            PlayRun();
+        }
+        else
+        {
+            PlayIdle();
+        }
+    }
+    void PlayIdle()
+    {
+        m_Anim.Play("Idle");
+    }
+    void PlayRun()
+    {
+        m_Anim.Play("Run");
+    }
+}
